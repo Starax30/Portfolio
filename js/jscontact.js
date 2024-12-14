bc688c5cd989f1d5b02993d574c7ff20dba0e1ad
thierry-vanboxel <thierryvanboxel@hotmail.com>
2024-12-14 08:31:33 +0100

    1

--- js/jscontact.js
+++ js/jscontact.js
@@ -2,13 +2,13 @@ const currentYear = new Date().getFullYear();
 document.getElementById("year").textContent = currentYear;
 
 
-let kaart = L.map('kaart').setView([51.505, -0.09], 13);
+let kaart = L.map('kaart').setView([51.2288, 4.4213], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(kaart);
 
-L.marker([51.5, -0.09]).addTo(kaart)
+L.marker([51.22978, 4.4178]).addTo(kaart)
     .bindPopup('AP-hogeschool')
     .openPopup();
 
